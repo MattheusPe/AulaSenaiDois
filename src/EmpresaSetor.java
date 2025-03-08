274970b9489a332d7763c4f895e91180f83614de
MattheusPe <matheuspereira230321@gmail.com>
2025-03-08 16:48:11 -0300

    feat: Classe setores da empresa

---
+++ src/EmpresaSetor.java
@@ -0,0 +1,12 @@
+public class EmpresaSetor extends Funcionario {
+    String nomeSetorFinanca = "Financas";
+    String nomeRH = "RH";
+    String nomeSetorJuridico = "Juridico";
+    
+
+
+
+
+
+
+}
